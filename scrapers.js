bbdeb9e7aa538aaefceb0cf57be3ebd86fa4f36c
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-04 18:41:03 +0300

    Fix cronjob.

--- scrapers.js
+++ scrapers.js
@@ -10,7 +10,7 @@ getTimeSeries().catch((err) => logger.error(err))
 // Creates a cronjob which executes the latest report scraper
 // every 5 minutes between 6 and 10 pm.
 const getLatestReportJob = new CronJob(
-	'* */5 18-22 * * *',
+	'00 */5 18-22 * * *',
 	getLatestReport,
 	null,
 	false,
