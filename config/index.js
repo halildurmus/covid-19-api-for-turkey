019c40a3af71b3aa7e9daade5803a0a778fc91f2
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-06 16:02:44 +0300

    Use redis url when creating a connection to the redis.

--- config/index.js
+++ config/index.js
@@ -2,11 +2,9 @@ require('dotenv').config()
 
 module.exports = {
 	apiPrefix: process.env.API_PREFIX,
-	redisHost: process.env.REDIS_HOST,
-	redisPort: process.env.REDIS_PORT,
-	redisPassword: process.env.REDIS_PASSWORD,
 	redisKeyLatestReport: process.env.REDIS_KEY_LATEST_REPORT,
 	redisKeyTimeSeries: process.env.REDIS_KEY_TIME_SERIES,
+	redisUrl: process.env.REDIS_URL,
 	nodeEnv: process.env.NODE_ENV,
 	serverPort: process.env.SERVER_PORT,
 }
