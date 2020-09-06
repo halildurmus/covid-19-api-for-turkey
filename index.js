ec9f04ad0a42cb8aaf2aa30128f464c51893f524
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-06 16:30:24 +0300

    Remove env variable PORT from config file.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const { serverPort } = require('./config')
 const { logger } = require('./utils')
 const app = require('./app')
-const port = serverPort || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () =>
 	logger.info(`Server is up on port ${port}.`, { service: 'server' })
