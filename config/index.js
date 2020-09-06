20a1bbfcc82f537921e3888d09b375f139ccc095
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-06 16:28:18 +0300

    Change env variable SERVER_PORT to PORT.

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
 	apiPrefix: process.env.API_PREFIX,
+	nodeEnv: process.env.NODE_ENV,
+	port: process.env.PORT,
 	redisKeyLatestReport: process.env.REDIS_KEY_LATEST_REPORT,
 	redisKeyTimeSeries: process.env.REDIS_KEY_TIME_SERIES,
 	redisUrl: process.env.REDIS_URL,
-	nodeEnv: process.env.NODE_ENV,
-	serverPort: process.env.SERVER_PORT,
 }
