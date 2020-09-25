d874b9b139a8dc60b27b3622cb5b12a933feb5a5
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-25 21:18:33 +0300

    Add health check endpoint.

--- app.js
+++ app.js
@@ -16,11 +16,27 @@ app.use(express.urlencoded({ extended: true }))
 
 // Routes
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(null, swaggerOptions))
+app.use(apiPrefix, latestReportRouter)
+app.use(apiPrefix, timeSeriesRouter)
+
 app.get('/', async (req, res) => {
 	res.redirect('/docs')
 })
-app.use(apiPrefix, latestReportRouter)
-app.use(apiPrefix, timeSeriesRouter)
+
+app.get('/health', async (req, res) => {
+	const healthCheck = {
+		message: 'OK',
+		uptime: process.uptime(),
+		timestamp: Date.now(),
+	}
+
+	try {
+		res.send(healthCheck)
+	} catch (e) {
+		healthCheck.message = e
+		res.status(503).send()
+	}
+})
 
 // If the error is not an instanceOf APIError, convert it.
 app.use(error.converter)
